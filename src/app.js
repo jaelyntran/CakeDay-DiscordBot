a1565e6bb51f2329fdd7e4b74eb7ca67b52d58da
jaelyntran <70259055+jaelyntran@users.noreply.github.com>
2026-01-22 14:21:26 -0800

    Updated app.js, deploy-commands.js, and ping.js

--- src/app.js
+++ src/app.js
@@ -1,45 +1,52 @@
 import 'dotenv/config';
-import { REST, Routes } from 'discord.js';
 import fs from 'node:fs';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { Client, Collection, GatewayIntentBits } from 'discord.js';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const commands = [];
+const __dirname  = path.dirname(__filename);
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.commands = new Collection();
+client.autoSummaryChannels = {};
+client.messageBuffer = {};
+
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(f => f.endsWith('.js'));
 
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
+    console.log(filePath);
     const commandModule = await import(`file://${filePath}`);
     const command = commandModule.default || commandModule;
     if (command.data && command.execute) {
-    	commands.push(command.data.toJSON());
+        client.commands.set(command.data.name, command);
     } else {
-    	console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
     }
 }
 
-const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(f => f.endsWith('.js'));
 
-(async () => {
-	try {
-        console.log(`Started refreshing ${commands.length} application (/) commands.`);
-        if (process.env.GUILD_ID) {
-            await rest.put(
-                Routes.applicationGuildCommands(process.env.APP_ID, process.env.GUILD_ID),
-                { body: commands }
-            );
-            console.log(`Successfully reloaded ${commands.length} guild commands.`);
-        }
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  console.log(filePath);
+  const eventModule = await import(`file://${filePath}`);
+  const event = eventModule.default || eventModule;
 
-        await rest.put(
-            Routes.applicationCommands(process.env.APP_ID),
-            { body: commands }
-        );
-        console.log(`Successfully reloaded ${commands.length} global commands.`);
-    } catch (err) {
-        console.error(err);
-    }
-})();
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+client.login(process.env.DISCORD_TOKEN);
